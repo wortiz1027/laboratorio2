505e35d673fcc30b131542ad33c72c87f7ee9f9a
Eduardo Franco <eduardo.franco.rivera@gmail.com>
2020-05-09 10:26:44 -0500

    Proyecto para construcción de proveedores.

---
+++ proveedor/src/main/java/co/edu/javeriana/proveedor/ProveedorApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.proveedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProveedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProveedorApplication.class, args);
+	}
+
+}
---
+++ proveedor/src/test/java/co/edu/javeriana/proveedor/ProveedorApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.proveedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProveedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
