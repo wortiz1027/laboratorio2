11725df72f52748bbcc4c06118bc0ad3db1776f3
Eduardo Franco <eduardo.franco.rivera@gmail.com>
2020-05-10 23:43:59 -0500

    Agregando operación para envio de lista de cotizaciones al sistema de cotizaciones + cambio en el modelo de datos.

--- cotimiddleware/src/main/java/co/edu/javeriana/cotimiddleware/model/Cotizacion.java
+++ cotimiddleware/src/main/java/co/edu/javeriana/cotimiddleware/model/Cotizacion.java
@@ -1,9 +1,5 @@
 package co.edu.javeriana.cotimiddleware.model;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
@@ -11,31 +7,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "codigo")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idCotizacion")
 public class Cotizacion implements Serializable {
-    private static final long serialVersionUID = -1469181990232546386L;
+    private static final long serialVersionUID = 4081004502608822827L;
 
-    private String codigo;
-    private Date fechaCreacion;
-    private Date fechaRespuesta;
+    private BigDecimal idCotizacion;
+    private BigDecimal idUser;
     private List<Producto> productos;
-
-    public List<Producto> getProductos() {
-        if (this.productos == null)
-            this.productos = new ArrayList<Producto>(0);
-        return this.productos;
-    }
-
-    @Override
-    public String toString() {
-        return "Cotizacion{" +
-                "codigo='" + codigo + '\'' +
-                ", fechaCreacion=" + fechaCreacion +
-                ", fechaRespuesta=" + fechaRespuesta +
-                '}';
-    }
+    private Date fechaCotizacion;
+    private Date fechaRespuesta;
 }
