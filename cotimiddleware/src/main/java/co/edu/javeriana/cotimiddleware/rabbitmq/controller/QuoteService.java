b1796ae9371bc12a9dc4ca463cd9f3ff4b4c1e35
Wilman Ortiz <willman.ortiz@outlook.com>
2020-05-11 04:13:33 -0500

    Actualiza error deparseo json

--- cotimiddleware/src/main/java/co/edu/javeriana/cotimiddleware/rabbitmq/controller/QuoteService.java
+++ cotimiddleware/src/main/java/co/edu/javeriana/cotimiddleware/rabbitmq/controller/QuoteService.java
@@ -6,7 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,9 +27,9 @@ public class QuoteService {
     String routingkeysendquote;
 
     @PostMapping(path = "/enviarCotizacion",consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String enviarCotizacionesAProveedores(@RequestBody Cotizacion cotizacion) {
+    public ResponseEntity<Void> enviarCotizacionesAProveedores(@RequestBody Cotizacion cotizacion) {
         LOG.info("REST enviarCotizacionesAProveedores.cotizacion {}", cotizacion);
         rabbitMQSender.publishQuote(routingkeysendquote, cotizacion);
-        return "OK";
+        return new ResponseEntity<Void> (HttpStatus.OK);
     }
 }
--- securityserver/src/main/java/co/edu/javeriana/securityserver/utils/Main.java
+++ securityserver/src/main/java/co/edu/javeriana/securityserver/utils/Main.java
@@ -12,9 +12,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class Main {
 
     public static void main(String[] args) {
-        /*SecureRandom random = null;
-        //String clientId = "seguridad";
-        String clientId = "8de6d4c46d616eb4c358ba6f63bb54dc";
+        SecureRandom random = null;
+        String clientId = "Colombia2020";
+        //String clientId = "8de6d4c46d616eb4c358ba6f63bb54dc";
 
         try {
             random = SecureRandom.getInstance("SHA1PRNG");
@@ -25,9 +25,9 @@ public class Main {
         PasswordEncoder encoder = new BCryptPasswordEncoder();
 
         //for (int i = 0; i < 3; i++)
-        System.out.println("Hash {} --> " + encoder.encode(clientId));*/
+        System.out.println("Hash {} --> " + encoder.encode(clientId));
 
-        List<String> s = Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K");
+        /*List<String> s = Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K");
 
         List<List<String>> ss = new ArrayList<>();
 
@@ -59,7 +59,7 @@ public class Main {
 
         }
 
-        System.out.println("FIN " + ss.size());
+        System.out.println("FIN " + ss.size());*/
 
     }
 
